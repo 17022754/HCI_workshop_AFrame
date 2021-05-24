89d927f0836bd737a9b8efc4ac24816d892c6e06
17022754 <17022754@student.hhs.nl>
2021-05-24 14:19:42 +0200

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -1,18 +1,18 @@
 
 var rotationSpeed = 0.001;
-var mySelfmadeBox = document.getElementById('mySelfmadeBox');
+var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	mySelfmadeBox.object3D.rotation.x += rotationSpeed;
-	mySelfmadeBox.object3D.rotation.y += rotationSpeed;
-	mySelfmadeBox.object3D.rotation.z += rotationSpeed/2;
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+	myOtherBox.object3D.rotation.y += rotationSpeed;
+	myOtherBox.object3D.rotation.z += rotationSpeed/2;
 	//console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16);
 
 
-mySelfmadeBox.addEventListener('mouseenter', function(){
-	rotationSpeed = 0.01;
-	console.log('mouse enter');
-});
+// myOtherBox.addEventListener('mouseenter', function(){
+// 	rotationSpeed = 0.01;
+// 	console.log('mouse enter');
+// });
